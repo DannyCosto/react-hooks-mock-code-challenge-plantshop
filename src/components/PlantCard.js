87641b9e14d655a765ab162f9e8093b2e630ad6f
Danny <danny.costelloe@gmail.com>
2021-04-12 18:57:34 -0400

    1advanced

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, onDeletePlant}) {
+  const {id, name, image, price} = plant;
+  const [isInStock, setIsInStock] = useState(true)
+
+  function handleToggle(){
+    setIsInStock(!isInStock)
+  }
+
+  function handleDelete(){
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE",
+    })
+    onDeletePlant(id)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isInStock ? (
+        <button onClick = {handleToggle} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick = {handleToggle}>Out of Stock</button>
       )}
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
